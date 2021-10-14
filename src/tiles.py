e7172b14c2d6af4d92c5978a0676125e341acb73
Brandon Rodriguez <brodriguez8774@gmail.com>
2021-10-14 16:26:33 -0400

    Update tile wall rendering to show on environment edges

--- src/tiles.py
+++ src/tiles.py
@@ -33,7 +33,18 @@ class TileSet:
             # Initialize each tile in row.
             for col_index in range(sprite_data['sprite_w_count']):
                 x_coord = (col_index * 50) + sprite_data['max_pixel_left']
-                curr_row.append(Tile(sprite_factory, sprite_renderer, x_coord, y_coord, col_index, row_index))
+                curr_row.append(
+                    Tile(
+                        sprite_factory,
+                        sprite_renderer,
+                        window_data,
+                        sprite_data,
+                        x_coord,
+                        y_coord,
+                        row_index,
+                        col_index,
+                    )
+                )
 
             # Set full row to tile set.
             self.tile_set.append(curr_row)
@@ -43,15 +54,77 @@ class Tile:
     """
     A single tile, representing a single location in the environment.
     """
-    def __init__(self, sprite_factory, sprite_renderer, window_x_coord, window_y_coord, row_index, col_index):
-        self.renderer = sprite_renderer
+    def __init__(
+        self,
+        sprite_factory, sprite_renderer, window_data, sprite_data, window_x_coord, window_y_coord, row_index, col_index,
+    ):
+        self.sprite_factory = sprite_factory
+        self.sprite_renderer = sprite_renderer
+        self.window_data = window_data
+        self.sprite_data = sprite_data
         self.row_index = row_index
         self.col_index = col_index
         self.x = window_x_coord
         self.y = window_y_coord
 
-        # Render self to window.
+        # Render base tile background.
         sprite_background = sprite_factory.from_image(RESOURCES.get_path('background.png'))
         sprite_background.x = self.x
         sprite_background.y = self.y
         sprite_renderer.render(sprite_background)
+
+        print('x: {0}    y: {1}'.format(self.col_index, self.row_index))
+
+        # Handle for edge tile walls.
+        if self.row_index == 0:
+            self.display_north_wall()
+        elif self.row_index == (self.sprite_data['sprite_h_count'] - 1):
+            self.display_south_wall()
+        if self.col_index == 0:
+            self.display_west_wall()
+        elif self.col_index == (self.sprite_data['sprite_w_count'] - 1):
+            self.display_east_wall()
+
+    def display_north_wall(self):
+        """
+
+        :return:
+        """
+        print('    displaying north/top wall'.format(self.col_index, self.row_index))
+        wall_background = self.sprite_factory.from_image(RESOURCES.get_path('wall_north.png'))
+        wall_background.x = self.x
+        wall_background.y = self.y
+        self.sprite_renderer.render(wall_background)
+
+    def display_east_wall(self):
+        """
+
+        :return:
+        """
+        print('    displaying east/right wall'.format(self.col_index, self.row_index))
+        wall_background = self.sprite_factory.from_image(RESOURCES.get_path('wall_east.png'))
+        wall_background.x = self.x
+        wall_background.y = self.y
+        self.sprite_renderer.render(wall_background)
+
+    def display_south_wall(self):
+        """
+
+        :return:
+        """
+        print('    displaying south/bottom wall'.format(self.col_index, self.row_index))
+        wall_background = self.sprite_factory.from_image(RESOURCES.get_path('wall_south.png'))
+        wall_background.x = self.x
+        wall_background.y = self.y
+        self.sprite_renderer.render(wall_background)
+
+    def display_west_wall(self):
+        """
+
+        :return:
+        """
+        print('    displaying west/left wall'.format(self.col_index, self.row_index))
+        wall_background = self.sprite_factory.from_image(RESOURCES.get_path('wall_west.png'))
+        wall_background.x = self.x
+        wall_background.y = self.y
+        self.sprite_renderer.render(wall_background)
